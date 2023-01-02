9f91bf222d745505272740317877be8f7ab83ac5
Surbi <112757952+Surbifw262@users.noreply.github.com>
2023-01-02 22:03:09 +0530

    Add files via upload

---
+++ problem3.js
@@ -0,0 +1,6 @@
+//Create a variable name to store your name
+//Create a variable age to store your age
+//Print the values stored in the variable on one line followed by the type of the variable in the next line//
+let a="Name = Surabhi,";
+let b="age = 26,";
+console.log(a ,"\n",b);
---
+++ problem4.js
@@ -0,0 +1,21 @@
+//Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+let Report=`
+█▄─▄▄▀█▄─▄▄─█▄─▄▄─█─▄▄─█▄─▄▄▀█─▄─▄─███─▄▄▄─██▀▄─██▄─▄▄▀█▄─▄▄▀█
+██─▄─▄██─▄█▀██─▄▄▄█─██─██─▄─▄███─█████─███▀██─▀─███─▄─▄██─██─█
+▀▄▄▀▄▄▀▄▄▄▄▄▀▄▄▄▀▀▀▄▄▄▄▀▄▄▀▄▄▀▀▄▄▄▀▀▀▀▄▄▄▄▄▀▄▄▀▄▄▀▄▄▀▄▄▀▄▄▄▄▀▀`;
+let a="Name = Surabhi";
+let b="School Name = Govt.Girls School,Goluwala";
+let c="Roll No.= fw25_262";
+let d="Section = A";
+let G="Grade  A++";
+let s=`
+▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ `;
+let name= "Subject        HTML    CSS    JAVA";
+let n="Score           90      95     95  ";
+let closeline=`
+▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ `;
+console.log(Report,"\n","\n",a,"\n","\n",b,"\n","\n",c,"\n","\n",d,"\n","\n",G,"\n","\n",s,"\n","\n",name,"\n","\n",n,"\n",closeline);
+
+ 
+
+
